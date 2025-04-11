37eb3ed77ac6e63a2b4814bb391643a2bb2027f4
Arthur <arthurdk01@gmail.com>
2025-04-11 15:08:36 -0300

    update-tech-stack

---
+++ src/router.tsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./routes/HomePage";
+
+export const router = createBrowserRouter([
+    {path: "/", element: <HomePage />},
+    
+  ]);
---
+++ src/routes/HomePage.tsx
@@ -0,0 +1,5 @@
+const HomePage: React.FC = () => {
+  return <h2 className="text-4xl font-primary" >oi, teste</h2>;
+};
+
+export default HomePage;
